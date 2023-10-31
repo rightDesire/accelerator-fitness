d53b3edafd01ea26edff062cf207362a3b4cfbba
Egor <105357627+rightDesire@users.noreply.github.com>
2023-11-01 01:46:42 +0300

    Меняет реализацию видео в блоке gym

--- source/js/modules/gym-video.js
+++ source/js/modules/gym-video.js
@@ -1,13 +1,21 @@
 const gymVideo = () => {
-  const videoContainer = document.querySelector('.gym__video-wrapper');
+  const videoContainer = document.querySelector('[data-video-wrapper]');
   const videoPoster = videoContainer.querySelector('[data-poster]');
   const videoButton = videoContainer.querySelector('[data-button]');
-  const videoIframe = videoContainer.querySelector('[data-iframe]');
+
+  const createIframe = () => {
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://www.youtube.com/embed/9TZXsZItgdw';
+    iframe.width = '364';
+    iframe.height = '228';
+    iframe.title = 'Видео из тренажерного зала.';
+    videoContainer.appendChild(iframe);
+  };
 
   const onVideoButtonClick = () => {
+    createIframe();
     videoPoster.style.display = 'none';
     videoButton.style.display = 'none';
-    videoIframe.style.display = 'block';
   };
 
   videoButton.addEventListener('click', onVideoButtonClick);
