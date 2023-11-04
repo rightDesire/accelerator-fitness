41781dc103e7fdf87cf429b09e4f2eca0f111b70
Egor <105357627+rightDesire@users.noreply.github.com>
2023-11-04 21:57:29 +0300

    Добавляет реализацию навигации в блоке Footer

--- source/js/modules/reviews-slider.js
+++ source/js/modules/reviews-slider.js
@@ -2,12 +2,6 @@ export const initReviewSlider = (isLoop, breakpointSettings) => {
   const swiperWrapper = document.querySelector('[data-reviews-swiper]');
   const buttonPrev = document.querySelector('[data-reviews-swiper-btn-prev]');
   const buttonNext = document.querySelector('[data-reviews-swiper-btn-next]');
-  const slides = document.querySelectorAll('[data-reviews-slide]');
-  const slidesArray = [...slides];
-
-  slidesArray.forEach((slide) => {
-    slide.setAttribute('tabindex', '0');
-  });
 
   return new window.Swiper(swiperWrapper, {
     loop: isLoop,
